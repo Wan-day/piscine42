1e307ba9b43d4792d2bdfd5391ceaa0935898c41
Wan-day <boldino.dima@gmail.com>
2026-07-14 00:44:42 +0200

    Added ex06

---
+++ Dima/C06/ex01/ft_print_params.c
@@ -0,0 +1,26 @@
+#include <unistd.h>
+
+void	ft_print(char *str)
+
+void	ft_putstr(char *str)
+{
+	int	i;
+
+	i = 0;
+	while(str[i] != '\0')
+	{
+		write (1, &str[i], 1);
+		i++;
+	}
+	write (1, "\n", 1);
+}
+
+int	main(int argc, char **argv)
+{
+	if (argc > 0)
+	{
+		ft_putstr(argv[0]);
+	}
+	return(0);
+}
+
