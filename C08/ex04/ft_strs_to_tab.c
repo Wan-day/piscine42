ad0c26b6737271f9cbfd82db01a7ca5523f613e9
Dmitrii Boldinov <dboldino@c1r8s1.42prague.com>
2026-07-19 19:10:50 +0200

    AAAAAAAAA

---
+++ C08/ex04/ft_strs_to_tab.c
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strs_to_tab.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dboldino <dboldino@student.42prague.fr>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/07/19 19:08:51 by dboldino          #+#    #+#             */
+/*   Updated: 2026/07/19 19:10:32 by dboldino         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_stock_str.h"
+#include <stdlib.h>
+
+struct s_stock_str	*ft_strs_to_tab(int ac, char **av);
+
+struct s_stock_str	*ft_strs_to_tab(int ac, char **av)
+{
+
+}
